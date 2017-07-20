53a8c9c518a5e0f2302923c94cf2decebb8027fe
LiLi <lil313@ziroom.com>
2017-07-20 15:40:25 +0800

    first commit

---
+++ src/com/ll/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.ll.demo;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("This is my test！");
+    }
+
+}
